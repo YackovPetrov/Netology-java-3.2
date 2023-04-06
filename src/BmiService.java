8e9065ea2868583f55b251de723599365fbb4350
Yackov <yackov.irkyt@gmail.com>
2023-04-07 01:28:42 +0500

    3 commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public double calculate (double weight_kg, double height_m){
-        double index = weight_kg / (height_m * 2);
+        double index = weight_kg / (height_m * height_m);
         int result = (int)index;
         return result;
 
