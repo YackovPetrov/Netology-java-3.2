8d99e9394d3fe81ff3eda39ec3c892da6375595f
Yackov <yackov.irkyt@gmail.com>
2023-04-07 01:26:39 +0500

    2 commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,2 +1,8 @@
 public class BmiService {
+    public double calculate (double weight_kg, double height_m){
+        double index = weight_kg / (height_m * 2);
+        int result = (int)index;
+        return result;
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         BmiService service = new BmiService();
-
-        System.out.println("Hello world!");
+        double weight_kg = 98;
+        double height_m = 1.87;
+        double bmiindex = service.calculate(weight_kg, height_m);
+        System.out.println("Индекс массы тела: " + bmiindex);
     }
 }
