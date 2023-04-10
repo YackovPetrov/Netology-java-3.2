7da932b4ac231168b2530713346a2fc60bc15d56
Yackov <yackov.irkyt@gmail.com>
2023-04-10 21:46:26 +0500

    Corrected homework reformate code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-    public double calculate (double weight_kg, double height_m){
+    public double calculate(double weight_kg, double height_m) {
         double index = weight_kg / (height_m * height_m);
-        int result = (int)index;
+        int result = (int) index;
         return result;
 
     }
